48120b7c910f762e14c91c450a350b6179882183
Культяпов Константин <pozys@mail.ru>
2022-09-16 22:22:33 +0300

    fix bugs

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -2,7 +2,7 @@
 
 namespace Php\Project\Lvl2\Formatters\Json;
 
-function getFormatted(array $value)
+function getFormatted(array $value): string
 {
     return json_encode($value, JSON_PRETTY_PRINT);
 }
